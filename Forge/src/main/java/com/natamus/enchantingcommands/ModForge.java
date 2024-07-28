b82c96800d7915813b59a22f9cb0dc4c94ba2e47
Rick South <rick@natamus.com>
2024-07-28 07:35:14 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/enchantingcommands/ModForge.java
+++ Forge/src/main/java/com/natamus/enchantingcommands/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.enchantingcommands;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.enchantingcommands.forge.config.IntegrateForgeConfig;
 import com.natamus.enchantingcommands.forge.events.ForgeCommandRegisterEvent;
 import com.natamus.enchantingcommands.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
