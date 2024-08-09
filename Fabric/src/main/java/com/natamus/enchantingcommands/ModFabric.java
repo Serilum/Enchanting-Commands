628bdb1b0d93b1d034f0931eddf2242e0748811a
Rick South <rick@natamus.com>
2024-08-09 08:25:49 +0200

    Enchanting Commands source code for 1.21.1.

---
+++ Fabric/src/main/java/com/natamus/enchantingcommands/ModFabric.java
@@ -0,0 +1,35 @@
+package com.natamus.enchantingcommands;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.enchantingcommands.cmds.CommandEc;
+import com.natamus.enchantingcommands.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, commandBuildContext, environment) -> {
+			CommandEc.register(dispatcher, commandBuildContext);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
