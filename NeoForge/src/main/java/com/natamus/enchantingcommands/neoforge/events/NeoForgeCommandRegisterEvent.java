0207ffce56b37cd9415874e3ca36a00f1d49711f
Rick South <rick@natamus.com>
2025-02-12 23:36:02 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/enchantingcommands/neoforge/events/NeoForgeCommandRegisterEvent.java
+++ NeoForge/src/main/java/com/natamus/enchantingcommands/neoforge/events/NeoForgeCommandRegisterEvent.java
@@ -3,9 +3,7 @@ package com.natamus.enchantingcommands.neoforge.events;
 import com.natamus.enchantingcommands.cmds.CommandEc;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeCommandRegisterEvent {
 	@SubscribeEvent
 	public static void registerCommands(RegisterCommandsEvent e) {
