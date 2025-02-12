1055813d95d554469e5f2d799e75fd67ea360e29
Rick South <rick@natamus.com>
2025-02-12 23:35:58 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/enchantingcommands/forge/events/ForgeCommandRegisterEvent.java
+++ Forge/src/main/java/com/natamus/enchantingcommands/forge/events/ForgeCommandRegisterEvent.java
@@ -3,12 +3,10 @@ package com.natamus.enchantingcommands.forge.events;
 import com.natamus.enchantingcommands.cmds.CommandEc;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeCommandRegisterEvent {
     @SubscribeEvent
-    public void registerCommands(RegisterCommandsEvent e) {
+    public static void registerCommands(RegisterCommandsEvent e) {
     	CommandEc.register(e.getDispatcher(), e.getBuildContext());
     }
 }
