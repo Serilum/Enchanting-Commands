68a3f933cb289a4849d6d5da35adc2a67096050b
Rick South <rick@natamus.com>
2026-03-26 01:54:29 +0100

    Enchanting Commands source code for 26.1.0.

---
+++ Forge/src/main/java/com/natamus/enchantingcommands/forge/events/ForgeCommandRegisterEvent.java
@@ -0,0 +1,21 @@
+package com.natamus.enchantingcommands.forge.events;
+
+import com.natamus.enchantingcommands.cmds.CommandEc;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeCommandRegisterEvent {
+	public static void registerEventsInBus() {
+		// BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeCommandRegisterEvent.class);
+
+		RegisterCommandsEvent.BUS.addListener(ForgeCommandRegisterEvent::registerCommands);
+	}
+
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent e) {
+    	CommandEc.register(e.getDispatcher(), e.getBuildContext());
+    }
+}
